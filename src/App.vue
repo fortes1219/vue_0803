1e296b93ec53309072ceb8b95975f2da69156e63
fortes.huang <tokiya.kagami@gmail.com>
2019-07-27 13:09:10 +0800

    add vue.config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <p @click="$router.push({name: 'home'})">Home</p>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
