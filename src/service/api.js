687c998549def8e24b2e299ebe5627bca0606aab
fortes.huang <tokiya.kagami@gmail.com>
2019-10-11 11:41:16 +0800

    fixed promise function

--- src/service/api.js
+++ src/service/api.js
@@ -1,45 +1,45 @@
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:3000/'
 const api = {
-	headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 
-	get: function (url, params) {
-		return new Promise((resolve, reject) => {
-			axios.get(url, {
-				params: params
-			})
-				.then((response) => {
-					resolve(response.data);
-				})
-				.catch((error) => {
-					reject(error);
-				});
-		})
-	},
+  get: (url, params) => {
+    return new Promise((resolve, reject) => {
+      axios.get(url, {
+        params: params
+      })
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
 
-	post: function (url, params) {
-		return new Promise((resolve, reject) => {
-			axios.post(url, params)
-				.then((response) => {
-					resolve(response.data);
-				})
-				.catch((error) => {
-					reject(error);
-				});
-		})
-	},
+  post: (url, params) => {
+    return new Promise((resolve, reject) => {
+      axios.post(url, params)
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
 
-	delete: function (url, params) {
-		return new Promise((resolve, reject) => {
-			axios.delete(url)
-				.then((response) => {
-					resolve(response.data);
-				})
-				.catch((error) => {
-					reject(error);
-				});
-		})
-	}
+  delete: (url, params) => {
+    return new Promise((resolve, reject) => {
+      axios.delete(url)
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  }
 
 }
 export default api
