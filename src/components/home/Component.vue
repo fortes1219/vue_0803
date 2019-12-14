1e78e574f70ed2b7cb9a2a3f7f600b450993adfb
fortes.huang <tokiya.kagami@gmail.com>
2019-12-14 12:12:27 +0800

    editted

--- src/components/home/Component.vue
+++ src/components/home/Component.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="page component">
     <h1>父子組件資料傳遞</h1>
-    <br>
-    <tabs :durationsDisplay="true" @tabDuration="tabDuration"></tabs>
-		<!-- <tabs :onSaleDisplay="true" @tabOnSale="tabOnSale"></tabs> -->
-    <ve-line v-if="currentTab == 0" key="dayTabs01" :data="betSuccess" :loading="true" :settings="lineChartConfig" :legend-visible="false" :colors="lineChartColor.blue" />
+    <br />
+    <tabs
+      :durationsDisplay="true" 
+      @tabDuration="tabDuration" 
+    />
+
+    <tabs
+      :onSaleDisplay="true" 
+      @tabOnSale="tabOnSale" 
+    />
+    <!-- <tabs :onSaleDisplay="true" @tabOnSale="tabOnSale"></tabs> -->
+    <ve-line
+      v-if="currentTab == 0"
+      key="dayTabs01"
+      :data="betSuccess"
+      :loading="true"
+      :settings="lineChartConfig"
+      :legend-visible="false"
+      :colors="lineChartColor.blue"
+    />
   </div>
 </template>
 <script>
-import tabs from '../tabs'
+import tabs from "../tabs"
 export default {
   components: {
     tabs
@@ -19,43 +35,41 @@ export default {
       area: true
     }
     return {
-      currentTab: '',
+      currentTab: "",
       lineChartColor: {
-        blue: ['#dcb5ff'],
-        orange: ['#a5a552'],
-        green: ['#ffe153']
+        blue: ["#dcb5ff"],
+        orange: ["#a5a552"],
+        green: ["#ffe153"]
       },
       betSuccess: {
-        columns: ['日期', '有效投注'],
+        columns: ["日期", "有效投注"],
         rows: [
-          { '日期': '08/17', '有效投注': '100000' },
-          { '日期': '08/18', '有效投注': '350000' },
-          { '日期': '08/19', '有效投注': '490000' },
-          { '日期': '08/20', '有效投注': '370000' },
-          { '日期': '08/21', '有效投注': '540000' },
-          { '日期': '08/22', '有效投注': '350000' },
-          { '日期': '08/23', '有效投注': '650000' },
-          { '日期': '08/24', '有效投注': '1000000' },
-          { '日期': '08/25', '有效投注': '1400000' },
-          { '日期': '08/26', '有效投注': '700000' }
+          { "日期": "08/17", "有效投注": "100000" },
+          { "日期": "08/18", "有效投注": "350000" },
+          { "日期": "08/19", "有效投注": "490000" },
+          { "日期": "08/20", "有效投注": "370000" },
+          { "日期": "08/21", "有效投注": "540000" },
+          { "日期": "08/22", "有效投注": "350000" },
+          { "日期": "08/23", "有效投注": "650000" },
+          { "日期": "08/24", "有效投注": "1000000" },
+          { "日期": "08/25", "有效投注": "1400000" },
+          { "日期": "08/26", "有效投注": "700000" }
         ]
-      },
+      }
     }
   },
 
   methods: {
     tabDuration(val) {
-			this.currentTab = val
-      console.log('duration type: ', val)
-		},
-		tabOnSale(val) {
-			this.currentTab = val
-      console.log('on sale type: ', val)
+      this.currentTab = val
+      console.log("duration type: ", val)
+    },
+    tabOnSale(val) {
+      this.currentTab = val
+      console.log("on sale type: ", val)
     }
   },
 
-  created() {
-
-  }
+  created() {}
 }
 </script>
--- src/components/tabs.vue
+++ src/components/tabs.vue
@@ -1,62 +1,83 @@
 <template>
-	<div class="row horizontal v_center">
-		<el-radio-group v-if="durationsDisplay" v-model="duration.select" @change="durationChange" key="duration">
-			<el-radio-button v-for="(item, index) in duration.items" :key="index" :label="item.label">{{ item.name }}</el-radio-button>
-		</el-radio-group>
-		<el-radio-group v-if="onSaleDisplay" v-model="onSale.select" @change="onSaleChange" key="onSale">
-			<el-radio-button v-for="(item, index) in onSale.items" :key="index" :label="item.label">{{ item.name }}</el-radio-button>
-		</el-radio-group>
-	</div>
+  <div>
+    <!--Duration-->
+    <el-radio-group
+      v-if="durationsDisplay"
+      v-model="duration.select"
+      @change="durationChange"
+      key="duration"
+    >
+      <el-radio-button
+        v-for="(item, index) in duration.items"
+        :key="index"
+        :label="item.label"
+      >{{ item.name }}</el-radio-button>
+    </el-radio-group>
+    
+    <!--On sale-->
+    <el-radio-group
+      v-if="onSaleDisplay"
+      v-model="onSale.select"
+      @change="onSaleChange"
+      key="onSale"
+    >
+      <el-radio-button
+        v-for="(item, index) in onSale.items"
+        :key="index"
+        :label="item.label"
+      >{{ item.name }}</el-radio-button>
+    </el-radio-group>
+  </div>
 </template>
 <script>
 export default {
-	name: 'tabs',
-	props: {
-		durationsDisplay: {
-			type: Boolean,
-			default: false
-		},
-		onSaleDisplay: {
-			type: Boolean,
-			default: false
-		}
-	},
-	data() {
-		return {
-			duration: {
-				select: 0,
-				items: [
-					{ label: 0, name: '本日' },
-					{ label: 1, name: '本週' },
-					{ label: 2, name: '本月' }
-				]
-			},
+  name: "tabs",
+  props: {
+    durationsDisplay: {
+      type: Boolean,
+      default: false
+    },
+    onSaleDisplay: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      duration: {
+        select: 0,
+        items: [
+          { label: 0, name: "本日" },
+          { label: 1, name: "本週" },
+          { label: 2, name: "本月" }
+        ]
+      },
 
-			onSale: {
-				select: 0,
-				items: [
-					{ label: 0, name: '推薦' },
-					{ label: 1, name: '人氣' },
-					{ label: 2, name: '最新' }
-				]
-			}
-		}
-	},
+      onSale: {
+        select: 0,
+        items: [
+          { label: 0, name: "推薦" },
+          { label: 1, name: "人氣" },
+          { label: 2, name: "最新" }
+        ]
+      }
+    }
+  },
 
-	methods: {
-		durationChange(val) {
-			this.duration.select = val
-			this.$emit('tabDuration', val)
-		},
-		onSaleChange(val) {
-			this.onSale.select = val
-			this.$emit('tabOnSale', val)
-		}
-	},
+  methods: {
+    durationChange(val) {
+      this.duration.select = val
+      this.$emit("tabDuration", val)
+    },
+    onSaleChange(val) {
+      this.onSale.select = val
+      this.$emit("tabOnSale", val)
+    }
+  },
 
-	created() {
-		this.durationChange(this.duration.select)
-		this.onSaleChange(this.onSale.select)
-	}
+  created() {
+    this.durationChange(this.duration.select)
+    this.onSaleChange(this.onSale.select)
+  }
 }
 </script>
