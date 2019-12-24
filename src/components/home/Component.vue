216a2e64e009a0adb6939f87ee7fd0afe2b60304
fortes.huang <fortes.huang@goldnetit.com>
2019-12-24 18:37:55 +0800

    editted

--- src/components/home/Component.vue
+++ src/components/home/Component.vue
@@ -3,23 +3,27 @@
     <h1>父子組件資料傳遞</h1>
     <br />
     <tabs
-      :durationsDisplay="true" 
-      @tabDuration="tabDuration" 
-    />
-
-    <tabs
-      :onSaleDisplay="true" 
-      @tabOnSale="tabOnSale" 
+      :durationsDisplay="true"
+      @tabDuration="tabDuration"
     />
     <!-- <tabs :onSaleDisplay="true" @tabOnSale="tabOnSale"></tabs> -->
     <ve-line
       v-if="currentTab == 0"
       key="dayTabs01"
-      :data="betSuccess"
+      :data="income"
       :loading="true"
       :settings="lineChartConfig"
       :legend-visible="false"
       :colors="lineChartColor.blue"
+    />
+    <ve-line
+      v-if="currentTab == 1"
+      key="dayTabs02"
+      :data="incomeRate"
+      :loading="true"
+      :settings="lineChartConfigPercentage"
+      :legend-visible="false"
+      :colors="lineChartColor.orange"
     />
   </div>
 </template>
@@ -33,6 +37,10 @@ export default {
   data() {
     this.lineChartConfig = {
       area: true
+    },
+    this.lineChartConfigPercentage = {
+      area: true,
+      yAxisType: ['percent']
     }
     return {
       currentTab: "",
@@ -41,19 +49,34 @@ export default {
         orange: ["#a5a552"],
         green: ["#ffe153"]
       },
-      betSuccess: {
-        columns: ["日期", "有效投注"],
+      income: {
+        columns: ["日期", "有效總收益"],
         rows: [
-          { "日期": "08/17", "有效投注": "100000" },
-          { "日期": "08/18", "有效投注": "350000" },
-          { "日期": "08/19", "有效投注": "490000" },
-          { "日期": "08/20", "有效投注": "370000" },
-          { "日期": "08/21", "有效投注": "540000" },
-          { "日期": "08/22", "有效投注": "350000" },
-          { "日期": "08/23", "有效投注": "650000" },
-          { "日期": "08/24", "有效投注": "1000000" },
-          { "日期": "08/25", "有效投注": "1400000" },
-          { "日期": "08/26", "有效投注": "700000" }
+          { "日期": "08/17", "有效總收益": "100000" },
+          { "日期": "08/18", "有效總收益": "350000" },
+          { "日期": "08/19", "有效總收益": "490000" },
+          { "日期": "08/20", "有效總收益": "370000" },
+          { "日期": "08/21", "有效總收益": "540000" },
+          { "日期": "08/22", "有效總收益": "350000" },
+          { "日期": "08/23", "有效總收益": "650000" },
+          { "日期": "08/24", "有效總收益": "1000000" },
+          { "日期": "08/25", "有效總收益": "1400000" },
+          { "日期": "08/26", "有效總收益": "700000" }
+        ]
+      },
+      incomeRate: {
+        columns: ['日期', '獲利率'],
+        rows: [
+          { "日期": '08/17', '獲利率': '-0.05' },
+          { "日期": '08/18', '獲利率': '0.03' },
+          { "日期": '08/19', '獲利率': '0.05' },
+          { "日期": '08/20', '獲利率': '0.03' },
+          { "日期": '08/21', '獲利率': '0.06' },
+          { "日期": '08/22', '獲利率': '0.025' },
+          { "日期": '08/23', '獲利率': '0.06' },
+          { "日期": '08/24', '獲利率': '0.1' },
+          { "日期": '08/25', '獲利率': '0.13' },
+          { "日期": '08/26', '獲利率': '0.0865' }
         ]
       }
     }
--- src/components/tabs.vue
+++ src/components/tabs.vue
@@ -1,32 +1,35 @@
 <template>
   <div>
     <!--Duration-->
-    <el-radio-group
-      v-if="durationsDisplay"
-      v-model="duration.select"
-      @change="durationChange"
-      key="duration"
-    >
-      <el-radio-button
-        v-for="(item, index) in duration.items"
-        :key="index"
-        :label="item.label"
-      >{{ item.name }}</el-radio-button>
-    </el-radio-group>
-    
+    <div v-if="durationsDisplay" key="duration" class="row vertical">
+      <el-radio-group
+        v-model="duration.select"
+        @change="durationChange"
+      >
+        <el-radio-button
+          v-for="(item, index) in duration.items"
+          :key="index"
+          :label="item.label"
+        >{{ item.name }}</el-radio-button>
+      </el-radio-group>
+      <h2 data-space="space-vertical">{{ headerText }}</h2>
+    </div>
+
     <!--On sale-->
-    <el-radio-group
-      v-if="onSaleDisplay"
-      v-model="onSale.select"
-      @change="onSaleChange"
-      key="onSale"
-    >
-      <el-radio-button
-        v-for="(item, index) in onSale.items"
-        :key="index"
-        :label="item.label"
-      >{{ item.name }}</el-radio-button>
-    </el-radio-group>
+    <div v-if="onSaleDisplay" key="onSale" class="row vertical">
+      <el-radio-group
+        v-if="onSaleDisplay"
+        v-model="onSale.select"
+        @change="onSaleChange"
+      >
+        <el-radio-button
+          v-for="(item, index) in onSale.items"
+          :key="index"
+          :label="item.label"
+        >{{ item.name }}</el-radio-button>
+      </el-radio-group>
+      <br>
+    </div>
   </div>
 </template>
 <script>
@@ -47,9 +50,8 @@ export default {
       duration: {
         select: 0,
         items: [
-          { label: 0, name: "本日" },
-          { label: 1, name: "本週" },
-          { label: 2, name: "本月" }
+          { label: 0, name: "有效總收益" },
+          { label: 1, name: "獲利率" },
         ]
       },
 
@@ -61,6 +63,21 @@ export default {
           { label: 2, name: "最新" }
         ]
       }
+    }
+  },
+
+  computed: {
+    headerText() {
+      let text = ''
+      switch (this.duration.select) {
+        case 0:
+          text = '有效總收益'
+          break
+        case 1:
+          text = '獲利率'
+          break
+      }
+      return text
     }
   },
 
