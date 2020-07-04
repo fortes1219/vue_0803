cf925e2221be263fb7f639e05af9ae9e94044b79
fortes.huang <tokiya.kagami@gmail.com>
2020-07-04 21:42:47 +0800

    fixing babel.config

--- src/utils/resetForm.js
+++ src/utils/resetForm.js
@@ -1,17 +1,17 @@
 export const resetForm = (target) => {
-	let reset = (obj) => {
-		Object.keys(obj).map(key => {
-			if (obj[key] === Object(obj[key])) {
-				reset(obj[key])
-				if (obj[key] instanceof Array) {
-					obj[key] = []
-				}
-				return
-			}
-			else {
-				obj[key] = ''
-			}
-		})
-	}
-	reset(target)
+  const reset = (obj) => {
+    Object.keys(obj).map(key => {
+      if (obj[key] === Object(obj[key])) {
+        reset(obj[key])
+        if (obj[key] instanceof Array) {
+          obj[key] = []
+        }
+        return
+      }
+      else {
+        obj[key] = ''
+      }
+    })
+  }
+  reset(target)
 }
